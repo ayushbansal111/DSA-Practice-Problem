dd5482a3ded4444271227a94e579bcac9a46acc1
Ayush Bansal <ayushbansal162@gmail.com>
2024-08-05 19:09:37 +0530

    Time: 2 ms (80.84%), Space: 7.6 MB (6.27%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        // base case
-        if (n <= 1)
-            return n;
-        vector<int> dp(n + 1, -1);
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
 
-        if (dp[n] != -1)
-            return dp[n];
-
-        dp[n] = fib(n - 1) + fib(n - 2);
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
         return dp[n];
     }
 };
